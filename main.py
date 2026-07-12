49e5558496dbee78ebdb48e7dd10b920b54dd7d5
Brian <NCPEPOYDS@GMAIL.COM>
2026-07-11 22:18:38 -0400

    updated settings_views to views

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from routes.fantasy.views import router as fantasy_views_router
 from routes.explore import router as explore_router
 from routes.basketball.league import router as basketball_league_router
 from routes.betting.views import router as betting_router
-from routes.settings.views_settings import router as settings_router
+from routes.settings.views import router as settings_router
 app.include_router(settings_router)
 import os
 
